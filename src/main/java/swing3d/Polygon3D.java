470c142e7b8244896d89f6b4bcd6f52b5d5b4667
marcusquirk <marcus@192.168.0.19>
2020-04-08 10:44:47 -0500

    Moved the layering of several polygons to the Polygon3D class

--- src/main/java/swing3d/Polygon3D.java
+++ src/main/java/swing3d/Polygon3D.java
@@ -8,16 +8,20 @@ import java.util.List;
 public class Polygon3D {
 
     private final List<Vector> vertices = new ArrayList<>();
+    private final int layers = 100;
 
-    public Polygon3D(int numberOfSides, double radius) {
+    public Polygon3D(int numberOfSides, double radius, double depth) {
+        for(int j = 0; j<layers; j++){
         for (int i = 0; i < numberOfSides; i++) {
             double fraction = ((double) i) / numberOfSides;
             double angle = fraction * 2.0 * Math.PI;
             double x = radius * Math.cos(angle);
             double y = radius * Math.sin(angle);
-            Vector v = new Vector(x, y, 0.0);
+            Vector v = new Vector(x, y-depth/2+(j*depth/layers), 0.0);
             this.vertices.add(v);
         } // for
+        }//for
+        
     } // Polygon3D( int, double )
 
     public void transform(Matrix4X4 m) {
@@ -28,18 +32,24 @@ public class Polygon3D {
 
     public Shape getShape() {
         GeneralPath path = new GeneralPath();
+        
+        int size = this.vertices.size()/layers;
 
-        Vector v = this.vertices.get(0);
+        for (int j = 0; j<layers; j++){
+        Vector v = this.vertices.get(0+j*size);
         double x = v.get(0);
         double y = v.get(1);
         path.moveTo(x, y);
 
-        for (int i = 1; i < this.vertices.size(); i++) {
-            v = this.vertices.get(i);
+        for (int i = 1; i < size; i++) {
+            v = this.vertices.get(i+j*size);
             x = v.get(0);
             y = v.get(1);
             path.lineTo(x, y);
         } // for
+        }//for
+
+
 
         path.closePath();
 
