ce72bc56d070375b355dc15c58ceb34adf2b1510
marcusquirk <marcus@192.168.0.19>
2020-04-10 10:41:57 -0500

    Implemented illumination
    The sides are illuminated based on a "light". Not working perfectly yet.

--- src/main/java/swing3d/SwingPanel.java
+++ src/main/java/swing3d/SwingPanel.java
@@ -15,11 +15,8 @@ import javax.swing.Timer;
 
 public class SwingPanel extends JPanel implements ActionListener {
 
-    private final int points = 8;
     private double centerX = 0.0;
     private double centerY = 0.0;
-    private final double minorRadius = 0.2;
-    private final double majorRadius = 0.3;
 
     private double deltaX = Math.random() / 20;
     private double deltaY = Math.random() / 20;
@@ -30,23 +27,25 @@ public class SwingPanel extends JPanel implements ActionListener {
     private Color color = Color.red;
     private List<Polygon3D> poly = new ArrayList<>();
     private int NUM_POLYGON = 1;
-    private int NUM_SIDES = 5;
+    private int NUM_SIDES = 3;
     private List<Matrix4X4> spinner = new ArrayList<>();
 
+    private Vector ILLUMINATION_VECTOR = new Vector(-1, -1, 0);
+
     public SwingPanel() {
-        Timer timer = new Timer(20, this);
+        Timer timer = new Timer(40, this);
         timer.start();
 
         //create a spinner matrix
         Matrix4X4 a = new Matrix4X4();
-        a.rotationX(0.00);
+        a.rotationX(0.01);
         Matrix4X4 b = new Matrix4X4();
         b.rotationY(0.01);
         Matrix4X4 c = new Matrix4X4();
-        c.rotationZ(0.02);
+        c.rotationZ(0.025);
 
         for (int i = 0; i < NUM_POLYGON; i++) {
-            this.poly.add(new Polygon3D(NUM_SIDES, 0.3, 0.2));
+            this.poly.add(new Polygon3D(NUM_SIDES, 0.6, 1));
             this.spinner.add(a.multiply(b).multiply(c));
         }//for
 
@@ -91,40 +90,7 @@ public class SwingPanel extends JPanel implements ActionListener {
         transform.concatenate(translation);
         transform.concatenate(rotation);
 
-        List<Shape> s = new ArrayList<>();
-
-        if (poly.get(0).getTopZ() < poly.get(0).getBottomZ()) {
-            this.shape = poly.get(0).getShapeBottom();
-        } 
-        else {
-            this.shape = poly.get(0).getShapeTop();
-        }
-        s.add(transform.createTransformedShape(this.shape));
-        g2D.setColor(this.getColour());
-        g2D.fill(s.get(0));
-
-        for (int i = 0; i < NUM_SIDES - 1; i++) {
-            this.shape = poly.get(0).getShapeSide(i);
-            s.add(transform.createTransformedShape(this.shape));
-            g2D.setColor(new Color(this.getColour().getRed() + 30, this.getColour().getGreen(),
-                    this.getColour().getBlue() - 30));
-            g2D.fill(s.get(i + 1));
-        }//for
-        this.shape = poly.get(0).getShapeFinalSide();
-        s.add(transform.createTransformedShape(this.shape));
-        g2D.setColor(new Color(this.getColour().getRed() + 30, this.getColour().getGreen(),
-                this.getColour().getBlue() - 30));
-        g2D.fill(s.get(NUM_SIDES));
-
-        if (poly.get(0).getTopZ() > poly.get(0).getBottomZ()) {
-            this.shape = poly.get(0).getShapeBottom();
-        } 
-        else {
-            this.shape = poly.get(0).getShapeTop();
-        }
-        s.add(transform.createTransformedShape(this.shape));
-        g2D.setColor(this.getColour());
-        g2D.fill(s.get(NUM_SIDES + 1));
+        draw3DShape(transform, g2D, poly.get(0));
 
     } // paintComponent( Graphics )
 
@@ -136,5 +102,101 @@ public class SwingPanel extends JPanel implements ActionListener {
         }//for
         this.repaint();
     } // actionPerformed( ActionEvent )
+
+    public void draw3DShape(AffineTransform transform, Graphics2D g2D,
+            Polygon3D poly) {
+        List<Shape> s = new ArrayList<>();
+        Boolean TopZ = poly.getTopZ() < poly.getBottomZ();
+        if (TopZ) {
+            this.shape = poly.getShapeBottom();
+            g2D.setColor(getIlluminationBottom(poly));
+        } else {
+            this.shape = poly.getShapeTop();
+            g2D.setColor(getIlluminationTop(poly));
+        }
+        s.add(transform.createTransformedShape(this.shape));
+        g2D.fill(s.get(0));
+
+        for (int i = 0; i < NUM_SIDES - 1; i++) {
+            this.shape = poly.getShapeSide(i);
+            s.add(transform.createTransformedShape(this.shape));
+            g2D.setColor(getIlluminationSide(poly, i));
+            g2D.fill(s.get(i + 1));
+        }//for
+
+        this.shape = poly.getShapeFinalSide();
+
+        s.add(transform.createTransformedShape(this.shape));
+        g2D.setColor(getIlluminationFinalSide(poly));
+        g2D.fill(s.get(NUM_SIDES));
+
+        if (TopZ != true) {
+            this.shape = poly.getShapeBottom();
+            g2D.setColor(getIlluminationBottom(poly));
+            s.add(transform.createTransformedShape(this.shape));
+            g2D.fill(s.get(NUM_SIDES + 1));
+//            int topSide = poly.getBottomTopVertex();
+//            g2D.setColor(getIlluminationSide(poly, topSide));
+//            g2D.fill(s.get(topSide + 1));
+
+        } else {
+            this.shape = poly.getShapeTop();
+            g2D.setColor(getIlluminationTop(poly));
+            s.add(transform.createTransformedShape(this.shape));
+            g2D.fill(s.get(NUM_SIDES + 1));
+//            int topSide = poly.getTopTopVertex();
+//            g2D.setColor(getIlluminationSide(poly, topSide));
+//            g2D.fill(s.get(topSide + 1));
+        }
+
+    }
+
+    public Color getIlluminationTop(Polygon3D poly) {
+        double illFactor = poly.getNormalTop().dot(ILLUMINATION_VECTOR);
+        if (illFactor < 0.01) {
+            illFactor = 0.05;
+        } else if (illFactor > 1) {
+            illFactor = 1;
+        }
+        return new Color((int) (this.getColour().getRed() * illFactor),
+                (int) (this.getColour().getGreen() * illFactor),
+                (int) (this.getColour().getBlue() * illFactor));
+    }
+
+    public Color getIlluminationBottom(Polygon3D poly) {
+        double illFactor = poly.getNormalBottom().dot(ILLUMINATION_VECTOR);
+        if (illFactor < 0.01) {
+            illFactor = 0.05;
+        } else if (illFactor > 1) {
+            illFactor = 1;
+        }
+        return new Color((int) (this.getColour().getRed() * illFactor),
+                (int) (this.getColour().getGreen() * illFactor),
+                (int) (this.getColour().getBlue() * illFactor));
+    }
+
+    public Color getIlluminationSide(Polygon3D poly, int firstSide) {
+        double illFactor = poly.getNormalSide(firstSide).dot(ILLUMINATION_VECTOR);
+        if (illFactor < 0.01) {
+            illFactor = 0.01;
+        } else if (illFactor > 1) {
+            illFactor = 1;
+        }
+        return new Color((int) (this.getColour().getRed() * illFactor),
+                (int) (this.getColour().getGreen() * illFactor),
+                (int) (this.getColour().getBlue() * illFactor));
+    }
+
+    public Color getIlluminationFinalSide(Polygon3D poly) {
+        double illFactor = poly.getNormalFinalSide().dot(ILLUMINATION_VECTOR);
+        if (illFactor < 0.01) {
+            illFactor = 0.01;
+        } else if (illFactor > 1) {
+            illFactor = 1;
+        }
+        return new Color((int) (this.getColour().getRed() * illFactor),
+                (int) (this.getColour().getGreen() * illFactor),
+                (int) (this.getColour().getBlue() * illFactor));
+    }
 
 } // SwingPanel
